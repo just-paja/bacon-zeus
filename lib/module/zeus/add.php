5364ce758e2e763cf9aa60debb98dac8bc8dcbf3
just-paja <just.paja@gmail.com>
2014-02-17 21:36:44 +0100

    Update, add

--- lib/module/zeus/add.php
+++ lib/module/zeus/add.php
@@ -3,13 +3,12 @@
 $status = 400;
 $post = $request->post();
 $gps = explode(',', $request->post('gps'));
+$loc = \System\Json::decode(html_entity_decode($post['loc']));
 $post['gps'] = new \System\Gps(array(
 	'lat' => $gps[0],
 	'lng' => $gps[1],
 ));
 
-v($post);
-exit;
 $visitor = create('Zeus\Visitor', $post);
 
 if ($visitor) {
